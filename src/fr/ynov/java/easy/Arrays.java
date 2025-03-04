4347ee1ae24fdb2df445b4fc2bdea24852073336
Matteo <matteo.marquant@ynov.com>
2025-03-04 16:39:17 +0100

    exercice(Arrays): add Arrays class

---
+++ src/fr/ynov/java/easy/Arrays.java
@@ -0,0 +1,10 @@
+package fr.ynov.java.easy;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] i = {1,2,3,4,5,6,7,8,9,10};
+        for (int c : i) {
+            System.out.println(c);
+        }
+    }
+}
