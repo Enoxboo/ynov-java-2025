242114f5cc8cb328e11eef7d0d38b77f9d8f5ada
Matteo <matteo.marquant@ynov.com>
2025-03-04 17:03:27 +0100

    exercice(Scanners): add EvenOrOdd class

--- src/fr/ynov/java/easy/EvenOrOdd.java
+++ src/fr/ynov/java/easy/EvenOrOdd.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class EvenOrOdd {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number and I'll tell you if it's an even or odd number");
 
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         if (n % 2 == 0) {
             System.out.println("Even");
