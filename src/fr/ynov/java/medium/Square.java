cc5038a47b569a3e25c62c13e4ec233fbe98938b
Matteo <matteo.marquant@ynov.com>
2025-03-11 15:06:47 +0100

    Square(Medium): Square method

---
+++ src/fr/ynov/java/medium/Square.java
@@ -0,0 +1,12 @@
+package fr.ynov.java.medium;
+
+public class Square {
+    public static void main(String[] args) {
+
+    }
+
+    public static int square(int num) {
+        num *= num;
+        return num;
+    }
+}
