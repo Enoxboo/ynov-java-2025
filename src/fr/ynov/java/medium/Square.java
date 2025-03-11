49cc67244d90e48e0dc25a0372639618b4e50490
Matteo <matteo.marquant@ynov.com>
2025-03-11 15:08:39 +0100

    Square(Medium): read the number from the user

--- src/fr/ynov/java/medium/Square.java
+++ src/fr/ynov/java/medium/Square.java
@@ -1,8 +1,16 @@
 package fr.ynov.java.medium;
 
+import java.util.Scanner;
+
 public class Square {
     public static void main(String[] args) {
+        System.out.println("Enter a number and I'll tell you the square of it");
 
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = square(n);
+        System.out.println(result);
+        sc.close();
     }
 
     public static int square(int num) {
