59d8583bb727df95152c22c7434f0cf04f53053b
Matteo <matteo.marquant@ynov.com>
2025-03-11 16:01:33 +0100

    Person(Medium): Second part

---
+++ src/fr/ynov/java/medium/Quality.java
@@ -0,0 +1,21 @@
+package fr.ynov.java.medium;
+
+public class Quality {
+    private final int id;
+    private final String name;
+
+    public Quality(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Quality quality1 = new Quality(1,  "John Doe");
+        Quality quality2 = new Quality(1,   "John Doe");
+
+        final boolean twoEquals = quality1 == quality2;
+        System.out.println(twoEquals);
+        final boolean equalsMethod = quality1.equals(quality2);
+        System.out.println(equalsMethod);
+    }
+}
