103885e9e5417144a728f07502183c55af6de6f4
Matteo <matteo.marquant@ynov.com>
2025-03-14 15:30:10 +0100

    Palindrome

--- src/fr/ynov/java/medium/Palindrome.java
+++ src/fr/ynov/java/medium/Palindrome.java
@@ -1,4 +1,32 @@
 package fr.ynov.java.medium;
 
+import java.util.Scanner;
+
 public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a word and I'll tell you if it's a palindrome");
+        String word = scanner.nextLine();
+        boolean palindrome = palindrome(word);
+        if (palindrome) {
+            System.out.println("C'est bien un palindrome");
+        } else {
+            System.out.println("C'est pas un palindrome");
+        }
+    }
+
+    public static boolean palindrome(String word) {
+        word = word.toLowerCase();
+        int left = 0;
+        int right = word.length() - 1;
+
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 }
